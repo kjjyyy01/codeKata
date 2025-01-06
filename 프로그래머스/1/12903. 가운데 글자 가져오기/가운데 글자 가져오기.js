783f38d547fd4684ab4c1488f615862e6390600d
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-01-06 09:36:59 +0900

    삼항연산자 풀이 추가

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -11,3 +11,10 @@ function solution(s) {
 }
 console.log(solution("abcde"));
 console.log(solution("qwer"));
+
+//function solution(s) {
+//    const answer = s.length % 2 === 0 ? s[s.length / 2 - 1] + s[s.length / 2] : s[Math.floor(s.length / 2)];
+//     return answer;
+// }
+// console.log(solution("abcde"))
+// console.log(solution("qwer"))
