3f3d1e6a1616313874da832aaed59af844857845
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-01-13 09:37:39 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.19 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -0,0 +1,10 @@
+function solution(price, money, count) {
+    var answer = 0;
+
+    for (let i = 1; i <= count; i++) {
+        answer += price * i;
+    }
+
+    return answer > money ? answer - money : 0;
+}
+console.log(solution(3, 20, 4));
