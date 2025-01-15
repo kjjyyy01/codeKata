97bc576f03b1a223356fd70ae5883382b269fd08
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-01-15 10:06:07 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,11 +1,7 @@
 function solution(seoul) {
-    var answer = '';
     
-    var index = seoul.indexOf("Kim"); //"Kim"의 위치를 찾는다.
+    let x = seoul.indexOf("Kim")
     
-    answer = `김서방은 ${index}에 있다` ;
-    
-    
-    return answer;
+    return `김서방은 ${x}에 있다`;
 }
-console.log(solution(["Jane","Kim"])); // 김서방은 1에 있다
+console.log(solution(["Jane","Kim"]));
