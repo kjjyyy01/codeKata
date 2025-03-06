79acecc7c6bd74568e759bbf0e3ae8fa2599c591
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-06 09:11:08 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -0,0 +1,12 @@
+function solution(num1, num2) {
+    var answer = 0;
+    return answer = num1===num2 ? 1 : -1;
+    // if(num1===num2){
+    //     return answer = 1
+    // }else {
+    //     return answer = -1
+    // }
+}
+console.log(solution(2,3))
+console.log(solution(11,11))
+console.log(solution(7,99))
