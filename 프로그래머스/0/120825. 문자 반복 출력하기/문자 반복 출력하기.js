17ecbf8aaf5b0aca00545b997026af04398ab3e7
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-11 09:48:51 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,14 @@
+function solution(my_string, n) {
+    var answer = '';
+    
+    // for(let i = 0; i < my_string.length; i++){
+    //     for(let k = 0; k < n; k++)
+    //     answer += my_string[i]
+    // }
+    
+    for(let i = 0; i < my_string.length; i++){
+        answer += my_string[i].repeat(n)
+    }
+    
+    return answer;
+}
