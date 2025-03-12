99bf94ff518dc930fcda4d02e75a4831656d6236
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-12 09:36:20 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -3,6 +3,7 @@ function solution(my_string) {
     
     //문자열을 배열로만들고, 배열을 reverse로 뒤집고, join으로 문자열로
     answer = Array.from(my_string).reverse().join('')
+    // answer = [...my_string].reverse().join('')
     
     return answer;
 }
