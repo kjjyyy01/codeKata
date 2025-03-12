cf14d44cdafbf8cb779078e2f4bfc0eb4673ce61
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-12 09:24:07 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,16 +1,20 @@
 function solution(num_list) {
-    var answer = [];
-    let evenCount = 0;
-    let oddCount = 0;
+    // var answer = [];
+//     let evenCount = 0;
+//     let oddCount = 0;
     
-    for(let i = 0; i < num_list.length; i++){
-        if(num_list[i] % 2 === 0){
-            evenCount++
-        }else{
-            oddCount++
-        }
-    }
+//     for(let i = 0; i < num_list.length; i++){
+//         if(num_list[i] % 2 === 0){
+//             evenCount++
+//         }else{
+//             oddCount++
+//         }
+//     }
+    let answer = [
+        num_list.filter((num)=>num%2===0).length,
+        num_list.filter((num)=>num%2===1).length
+    ]
     
-    
-    return answer = [evenCount, oddCount];
+    return answer;
+    // return answer = [evenCount, oddCount];
 }
