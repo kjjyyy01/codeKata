bcd18e3de023682454f141cb40807c5b3a5ca2ad
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-13 09:49:41 +0900

    [level 0] Title: 배열의 평균값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -11,5 +11,3 @@ function solution(numbers) {
     // return answer = accNumber / numbers.length;
      return answer;
 }
-console.log(solution([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-console.log(solution([89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]))
