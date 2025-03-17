0bfe00ac7a824ba5e4274adbc07db9ea9c9d6678
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-17 09:40:00 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,9 +1,7 @@
 function solution(s1, s2) {
     let answer = 0;
-
-    answer = s1.filter((e) => s2.includes(e)).length;
-
-    return answer;
+    
+    answer = s1.filter((element)=>s2.includes(element));
+    
+    return answer.length;
 }
-console.log(solution(["a", "b", "c"], ["com", "b", "d", "p", "c"]));
-console.log(solution(["n", "omg"], ["m", "dot"]));
