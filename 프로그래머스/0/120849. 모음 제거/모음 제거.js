748235d8acf335dd767ed5f434f2647f6fd9df58
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-03-19 10:06:31 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -4,6 +4,7 @@ function solution(my_string) {
     
     const arrMyString = [...my_string];
     const filteredMyString = arrMyString.filter((element) => !k.includes(element)).join("");
-    
+    // return my_string.replace(/[aeiou]/g, '');
+
     return answer = filteredMyString;
 }
