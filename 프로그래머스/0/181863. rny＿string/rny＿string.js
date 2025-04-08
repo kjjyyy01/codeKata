d4bde9d2db16e54c908476770dc5989667e023b1
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-04-08 09:15:40 +0900

    [level 0] Title: rny_string, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181863. rny＿string/rny＿string.js
+++ 프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,4 +1,5 @@
 function solution(rny_string) {
     let answer = '';
-    return answer = rny_string.replace(/m/g,"rn")
+    // return answer = rny_string.replace(/m/g,"rn")
+    return answer = rny_string.replaceAll("m","rn");
 }
