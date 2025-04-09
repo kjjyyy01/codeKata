be83811086613f510271a283082c0babe7a439cb
JongYeon Kim <169040000+kjjyyy01@users.noreply.github.com>
2025-04-09 09:19:45 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.js
@@ -1,4 +1,4 @@
 function solution(my_string) {
     let answer = [];
-    return answer = my_string.split(" ").filter((element) => element.length >= 1);
+    return answer = my_string.split(" ").filter((element) => element);
 }
