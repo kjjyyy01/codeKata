30cae447cbe6b105501356ba59d5a380e02b523d
Jongyeon <169040000+kjjyyy01@users.noreply.github.com>
2025-05-08 22:18:26 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -11,10 +11,9 @@ function solution(num_list) {
     return -1;
 }
 
- // for(let i = 0; i < num_list.length; i++) {
- //        if(num_list[i] < 0) {
- //            answer = i;
- //        }else {
- //            answer = -1;
- //        }
- //    }
+// const solution = num_list => num_list.findIndex(v => v < 0)
+
+// function solution(num_list) {
+//     let a = num_list.filter(c=> {if(c<0) return c})
+//     return num_list.indexOf(a[0])
+// }
