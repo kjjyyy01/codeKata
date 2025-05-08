2ed4dd5fc800385565c27868ff3273b49c2e1813
Jongyeon <169040000+kjjyyy01@users.noreply.github.com>
2025-05-08 21:16:15 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.96 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -6,3 +6,10 @@ function solution(arr, intervals) {
     
     return answer = aArray.concat(bArray);
 }
+
+
+// function solution(arr, intervals) {
+//     const [[a,b],[c,d]] = intervals;
+
+//     return [...arr.slice(a, b+1), ...arr.slice(c, d+1)];
+// }
